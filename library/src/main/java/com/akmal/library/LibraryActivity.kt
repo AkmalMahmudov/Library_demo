8e4d6faa17e93f125a7be84ea5cd1ca957564181
akmal <akmal@olcha.uz>
2023-04-06 16:32:36 +0500

    fix

--- library/src/main/java/com/akmal/library/LibraryActivity.kt
+++ library/src/main/java/com/akmal/library/LibraryActivity.kt
@@ -1,18 +1,23 @@
 package com.akmal.library
 
 import android.accounts.AuthenticatorException
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.akmal.library.databinding.ActivityLibraryBinding
 
 class LibraryActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityLibraryBinding::bind)
+
+    @RequiresApi(Build.VERSION_CODES.ECLAIR)
     override fun onCreate(savedInstanceState: Bundle?) {
         if (!applicationContext.packageName.startsWith("com.akmal")) {
             throw AuthenticatorException("f**k off mate")
         } else {
+            Toast.makeText(this, "version:1.1", Toast.LENGTH_SHORT).show()
             initCLickActions()
         }
         super.onCreate(savedInstanceState)
